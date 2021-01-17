fb3beb96d58ee8bfcbdb7748cb02c8a978544586
BiloCode <billy_paredes_aycho@hotmail.com>
2021-01-17 12:51:34 -0500

    Correcion de tipado en el router de los paises

--- src/api/router/country_router.ts
+++ src/api/router/country_router.ts
@@ -1,7 +1,7 @@
-import { Application } from "express";
+import { Router } from "express";
 
 import getCountries from "../controllers/getCountries";
 
-export default (app : Application) => {
+export default (app : Router) => {
   app.get('/countries', getCountries);
 }
